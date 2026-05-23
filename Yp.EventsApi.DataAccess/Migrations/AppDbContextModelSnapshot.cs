09f7ad1a415747605e9f111e883c9340c94253db
kalev <vinderman771@gmail.com>
2026-05-23 14:57:30 +0300

    фикс версий

--- Yp.EventsApi.DataAccess/Migrations/AppDbContextModelSnapshot.cs
+++ Yp.EventsApi.DataAccess/Migrations/AppDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Yp.EventsApi.DataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.27")
+                .HasAnnotation("ProductVersion", "8.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
